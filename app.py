209689ab85e616ba399f73d73bf5001e9be90401
AaronEze <Aaron.Ezequiel.Busto@gmail.com>
2026-06-08 09:52:41 -0300

    Fix de app.py

--- app.py
+++ app.py
@@ -33,6 +33,8 @@ def create_app():
         
     return app
 
+# Creamos la instancia global para que Gunicorn pueda usarla, y también para correrla localmente con python app.py
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True, port=5000)
